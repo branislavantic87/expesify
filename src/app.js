330b73223523e5fdecd028e7a31d450e8152b99b
Branislav Antic <branislav.antic@cadev.rs>
2018-03-07 10:29:13 +0100

    gotov 99

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Watter bill' }))
+store.dispatch(addExpense({ description: 'Gas bill' }))
+store.dispatch(setTextFilter('gas'))
+
+const state = store.getState();
+const visibleExpanses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpanses)
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
